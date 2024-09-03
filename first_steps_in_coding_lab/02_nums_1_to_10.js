6cac26f4126471341b5afbf2875ac667856ef6f3
GeorgiDN <go6odn28@gmail.com>
2024-09-03 20:21:37 +0300

    added more solutions

---
+++ first_steps_in_coding_lab/02_nums_1_to_10.js
@@ -0,0 +1,25 @@
+// function nums1to20() {
+//     console.log(1)
+//     console.log(2)
+//     console.log(3)
+//     console.log(4)
+//     console.log(5)
+//     console.log(6)
+//     console.log(7)
+//     console.log(8)
+//     console.log(9)
+//     console.log(10)
+// }
+//
+// // nums1to20()
+
+
+
+//  --------------------------------------------  //
+function nums1to20() {
+    for (let i=1; i < 11; i++) {
+        console.log(i)
+    }
+}
+
+// nums1to20()
