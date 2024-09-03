dd103eca1b7ed8a078f3b29b0a2b885ce3ab0773
GeorgiDN <go6odn28@gmail.com>
2024-09-03 21:24:45 +0300

    added more solutions

---
+++ first_steps_in_coding_lab/04_inches_to_centimeters.js
@@ -0,0 +1,7 @@
+function convertInchesToCentimeters(input) {
+    let x = Number(input[0]);
+    let convertedNumber = x * 2.54
+    console.log(convertedNumber)
+}
+
+// convertInchesToCentimeters("5")
