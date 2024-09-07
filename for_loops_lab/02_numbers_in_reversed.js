0583beecbef200d41e3b86c5432bfa1fc149c6b2
GeorgiDN <go6odn28@gmail.com>
2024-09-07 16:23:47 +0300

    added more solutions

---
+++ for_loops_lab/02_numbers_in_reversed.js
@@ -0,0 +1,8 @@
+function numberReversed(input) {
+    let n = Number(input[0])
+    for (let num = n; num >= 1; num -=1) {
+        console.log(num)
+    }
+}
+
+// numberReversed(["2"])
---
+++ for_loops_lab/03_numbers_one_to_n_step_3.js
@@ -0,0 +1,8 @@
+function numberOneToNStep3(input) {
+    let n = Number(input[0])
+    for (let num = 1; num <= n; num +=3) {
+        console.log(num)
+    }
+}
+
+// numberOneToNStep3(["10"])
